59237a660f256d9c44e9be708122f521bc46285e
Cast <outcast8090@gmail.com>
2023-10-06 22:05:41 +0800

    Update PressKeyOpenDoor.cs

--- Assets/Scripts/PressKeyOpenDoor.cs
+++ Assets/Scripts/PressKeyOpenDoor.cs
@@ -10,34 +10,34 @@ public class PressKeyOpenDoor : MonoBehaviour
     public AudioSource DoorOpenSound;
     public bool Action = false;
 
-    void Start ()
+    void Start()
     {
         Instruction.SetActive(false);
     }
-    void OnTriggerEnter (Collider collision)
+    void OnTriggerEnter(Collider collision)
     {
         if (collision.transform.tag == "Player")
         {
             Instruction.SetActive(true);
-            Action = true;  
+            Action = true;
         }
     }
-    void OnTriggerExit (Collider collision)
+    void OnTriggerExit(Collider collision)
     {
         Instruction.SetActive(false);
-        Action = false; 
+        Action = false;
     }
-    void Update () 
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            Instruction.SetActive(false);
             if (Action == true)
             {
                 Instruction.SetActive(false);
                 AnimeObject.GetComponent<Animator>().Play("DoorOpen");
                 AnimeObject.GetComponent<Animator>().Play("DoorOpen2");
                 ThisTrigger.SetActive(false);
-                DoorOpenSound.Play();
                 Action = false;
             }
         }
