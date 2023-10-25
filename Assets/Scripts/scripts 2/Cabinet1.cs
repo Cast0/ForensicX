5bea88474faab8b9d54de6441a3274efb82380d5
izumi <78188780+vanooze@users.noreply.github.com>
2023-10-25 20:32:24 +0800

    ANIMATION FOR CABINET IS DONE
    animation is fix now (press E to Open/Close the cabinet door)

--- Assets/Scripts/scripts 2/Cabinet1.cs
+++ Assets/Scripts/scripts 2/Cabinet1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cabinet : Interactable
+public class Cabinet1 : Interactable
 {
     [SerializeField]
     private GameObject cabinetdoor;
@@ -10,18 +10,18 @@ public class Cabinet : Interactable
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    protected override void Interact() 
+    protected override void Interact()
     {
         cabinetdooropen = !cabinetdooropen;
-        cabinetdoor.GetComponent<Animator>().SetBool("cabinetopen",cabinetdooropen);
+        cabinetdoor.GetComponent<Animator>().SetBool("cabinet1open", cabinetdooropen);
     }
 }
