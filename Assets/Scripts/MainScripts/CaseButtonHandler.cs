8b3bdf48ef9c7f40ffd9b88cb8f10e595e2d6c0b
AGAPI07 <104341224+AGAPI07@users.noreply.github.com>
2023-12-27 00:26:31 +0800

    reset case

--- Assets/Scripts/MainScripts/CaseButtonHandler.cs
+++ Assets/Scripts/MainScripts/CaseButtonHandler.cs
@@ -5,20 +5,35 @@ using UnityEngine.UI;
 
 public class CaseButtonHandler : MonoBehaviour
 {
-    [SerializeField]GameObject lockOverlay;
-    [SerializeField]Button button;
-    [SerializeField]int casenumber = 1;
-    private void Start() {
-        if(PlayerPrefs.GetInt("caseUnlocked",1)>=casenumber){
+    [SerializeField] GameObject lockOverlay;
+    [SerializeField] Button button;
+    [SerializeField] int casenumber = 1;
+    private void Start()
+    {
+
+        CaseReset.CaseResetted += HandleCaseButton;
+        HandleCaseButton();
+
+    }
+    private void OnDestroy()
+    {
+        CaseReset.CaseResetted -= HandleCaseButton;
+    }
+    private void HandleCaseButton()
+    {
+        if (PlayerPrefs.GetInt("caseUnlocked", 1) >= casenumber)
+        {
             //unlock this button
             button.enabled = true;
             lockOverlay.SetActive(false);
         }
-        else{
+        else
+        {
             button.enabled = false;
-            lockOverlay.SetActive(true);  
+            lockOverlay.SetActive(true);
         }
-        button.onClick.AddListener(()=>{
+        button.onClick.AddListener(() =>
+        {
             CaseData.currentCaseOpened = casenumber;
         });
     }
